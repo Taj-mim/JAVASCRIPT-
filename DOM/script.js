06af641fd2818f6272d46e4b1578513279886e36
Taj-mim <tajmim915@gmail.com>
2026-07-07 03:08:48 +0600

    Basic things of DOM

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+/*QUESTION:1  Create a H2 heading element with the text "Hello, DOM!" 
+.append "this is taj mim"to this text using js*/
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+//h2.innerText = "Hello, DOM!";
+h2.innerText += " This is Taj mim";
