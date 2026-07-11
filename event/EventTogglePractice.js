9f642b75c3bff7f1c9975f8000eefa6819a2c154
Taj-mim <tajmim915@gmail.com>
2026-07-12 02:18:49 +0600

    Toggling mode

---
+++ event/EventTogglePractice.js
@@ -0,0 +1,17 @@
+let btn=document.querySelector("#toggleButton");
+let body=document.querySelector("body");
+let mode="light";
+btn.addEventListener("click",()=>{
+    if(mode==="light"){
+        mode="dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else
+    {
+        mode="light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(mode);
+});
